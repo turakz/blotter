2a7f8459683e4522a03ae49f70d2b36b419d639e
turakz <zachary.turak@gmail.com>
2021-05-04 22:55:37 -0400

    adds explicit ctor deletions

--- strategies/strategy.h
+++ strategies/strategy.h
@@ -3,6 +3,11 @@
 namespace blotter {
     class strategy_base {
     public:
+        strategy_base() = default;
+        strategy_base(const strategy_base&) = delete;
+        strategy_base& operator=(const strategy_base&) = delete;
+        strategy_base(const strategy_base&&) = delete;
+        strategy_base& operator=(const strategy_base&&) = delete;
         virtual ~strategy_base() {}
         virtual void execute() = 0;
     };
